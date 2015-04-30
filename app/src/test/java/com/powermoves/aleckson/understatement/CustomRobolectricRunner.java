04e1e6886d21e3d6724012e63c11f083166531dc
nyamwaya <alecksonnyamwaya@gmail.com>
2015-04-29 22:51:51 -0500

    custom robolectric test runner

---
+++ app/src/test/java/com/powermoves/aleckson/understatement/CustomRobolectricRunner.java
@@ -0,0 +1,30 @@
+package com.powermoves.aleckson.understatement;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by aleckson on 4/28/2015.
+ */
+public class CustomRobolectricRunner extends RobolectricTestRunner {
+    public CustomRobolectricRunner(Class<?> testClass)
+            throws InitializationError {
+        super(testClass);
+        String buildVariant = (BuildConfig.FLAVOR.isEmpty()
+                ? "" : BuildConfig.FLAVOR+ "/") + BuildConfig.BUILD_TYPE;
+        String intermediatesPath = BuildConfig.class.getResource("")
+                .toString().replace("file:", "");
+        intermediatesPath = intermediatesPath
+                .substring(0, intermediatesPath.indexOf("/classes"));
+
+        System.setProperty("android.package",
+                BuildConfig.APPLICATION_ID);
+        System.setProperty("android.manifest",
+                intermediatesPath + "/manifests/full/"
+                        + buildVariant + "/AndroidManifest.xml");
+        System.setProperty("android.resources",
+                intermediatesPath + "/res/" + buildVariant);
+        System.setProperty("android.assets",
+                intermediatesPath + "/assets/" + buildVariant);
+    }
+}
